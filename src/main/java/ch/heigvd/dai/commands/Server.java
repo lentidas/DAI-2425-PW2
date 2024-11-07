dd266f5344dea7f76fa92f6c0335c50555240cfe
Gonçalo Heleno <goncalo.carvalheiroheleno@heig-vd.ch>
2024-11-07 20:33:05 +0100

    feat: add first prototype of the picocli commands

---
+++ src/main/java/ch/heigvd/dai/commands/Server.java
@@ -0,0 +1,37 @@
+/*
+ * Wheel Of Fortune - a Java server/client implementation of the television game
+ * Copyright (C) 2024 Pedro Alves da Silva, Gonçalo Carvalheiro Heleno
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.heigvd.dai.commands;
+
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+
+@CommandLine.Command(
+    name = "server",
+    description = "Start the server and listen for client connections."
+)
+public class Server implements Callable<Integer> {
+
+  @CommandLine.ParentCommand
+  private Root parent;
+
+  @Override
+  public Integer call() {
+    return 0;
+  }
+}
